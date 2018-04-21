754d281ba9cf41ad2654add758019b4201f82108
catchonme <izengdp@163.com>
2018-04-21 20:19:39 +0800

    增加检测类型函数

---
+++ src/lang/object.js
@@ -0,0 +1,35 @@
+(function () {
+   var _toString = Object.prototype.toString,
+       NULL_TYPE = 'null',
+       UNDEFINED_TYPE = 'undefined',
+       BOOLEAN_TYPE = 'boolean',
+       NUMBER_TYPE = 'number',
+       STRING_TYPE = 'string',
+       OBJECT_TYPE = 'object',
+       FUNCTION_CLASS = '[object Function]',
+       BOOLAEN_CLASS = '[object Boolean]',
+       NUMBER_CLASS = '[object Number]',
+       STRING_CLASS = '[object String]',
+       ARRAY_CLASS = '[object Array]',
+       DATE_CLASS = '[object Date]';
+
+   function Type(o) {
+       switch(o) {
+           case null : return NULL_TYPE;
+           case (void 0) : return UNDEFINED_TYPE;
+       }
+       var type = typeof o;
+       switch (type) {
+           case 'boolean' : return BOOLEAN_TYPE;
+           case 'number' : return NUMBER_TYPE;
+           case 'string' : return STRING_TYPE;
+       }
+       return OBJECT_TYPE;
+   }
+
+
+
+
+
+
+})()
