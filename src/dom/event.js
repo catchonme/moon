9bdf5dc5ce9dc59a8f102617764a0e0dd1efd496
catchme <izengdp@163.com>
2018-04-23 10:41:21 +0800

    update code

--- src/dom/event.js
+++ src/dom/event.js
@@ -1,5 +1,12 @@
 (function () {
 
+    function getKeyPressed(event) {
+        event = getEvent(event);
+        var code = event.keyCode;
+        var value = String.fromCharCode(code);
+        return {'code':code,'value':value}
+    }
+
     function getEvent(W3CEvent) {
         return W3CEvent || window.event;
     }
@@ -10,10 +17,45 @@
         return target;
     }
 
-    function getKeyPressed(event) {
+    function stopPropagation(event) {
         event = getEvent(event);
-        var code = event.keyCode;
-        var value = String.fromCharCode(code);
-        return {'code':code,'value':value}
+        if (event.stopPropagation) {
+            event.stopPropagation()
+        } else {
+            event.cancelBubble = true;
+        }
+    }
+
+    function preventDefault(event) {
+        event = getEvent(event);
+        if (event.preventDefault) {
+            event.preventDefault()
+        } else {
+            event.returnValue = false;
+        }
+    }
+
+    function stop(event) {
+        stopPropagation(event);
+        preventDefault(event);
+    }
+
+    function addEvent(node, type, handler) {
+        if (node.addEventListener) {
+            node.addEventListener(type, handler, false);
+            return true;
+        } else if (node.attachEvent) {
+            node.attachEvent('on'+type, handler)
+        } else {
+            node["on" + type] = handler;
+        }
+    }
+
+    function removeEvent(node, type, handler) {
+        if (node.removeEventListener) {
+            node.removeEventListener(type, handler, false);
+        } else if (node.detach) {
+            node.detachEvent("on"+type, handler)
+        }
     }
 })()
