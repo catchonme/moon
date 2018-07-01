7b58fbefc2cb09ea49dc80d378c5d2c7641c6639
catchonme <izengdp@163.com>
2018-07-01 21:56:19 +0800

    add isFunction

--- src/dom/Element-3.js
+++ src/dom/Element-3.js
@@ -6,3 +6,15 @@ if (document.readyState !== 'loading') {
 } else {
     document.addEventListener('DOMContentLoaded', eventHandler);
 }
+
+/**
+ * 检测传入的参数是不是 JavaScript 函数对象
+ */
+function isFunction(item) {
+    if (typeof item === 'function') {
+        return true;
+    }
+    // toString 后面不是要加 call 吗
+    var type = Object.prototype.toString(item);
+    return type === '[object Function]' || type === '[object GeneratorFunction]';
+}
