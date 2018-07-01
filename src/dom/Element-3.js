70ab84f9b75597a60029ca7b1a8a0637c847d12a
catchonme <izengdp@163.com>
2018-07-01 21:47:55 +0800

    add windows.ready

---
+++ src/dom/Element-3.js
@@ -0,0 +1,8 @@
+/**
+ * Document ready by DOMContentLoaded
+ */
+if (document.readyState !== 'loading') {
+    eventHandler();
+} else {
+    document.addEventListener('DOMContentLoaded', eventHandler);
+}
